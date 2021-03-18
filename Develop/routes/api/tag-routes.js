150153d374b4668bb031b6cc261923a0c87fd701
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-03-17 19:00:13 -0500

    allTag route working

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,26 +3,53 @@ const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get("/", (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get("/", async (req, res) => {
+  // find all tags and include its associated Product data
+  try {
+    const allTagData = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    if(!allTagData){
+      res.status(404).json({message: "Uh-oh! Tag not found!"})
+    }
+
+    res.status(200).json(allTagData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
+  try {
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
+  try {
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
